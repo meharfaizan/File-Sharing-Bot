7b023dbee347e395062b2c84a970b0525d615a06
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-09-26 18:41:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -34,6 +34,9 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
