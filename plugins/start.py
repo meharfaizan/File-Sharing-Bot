022fccbdc8358e5bcdca716498a18c1cb31f3450
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-09-29 00:53:47 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -36,7 +36,7 @@ async def start_command(client: Client, message: Message):
      if not await present_user(id):
           try:
               await add_user(id)
-           except:
+          except:
               pass
     text = message.text
     if len(text)>7:
