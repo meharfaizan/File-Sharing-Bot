f9390ef9252733eb95d2d7fa781b4dbdd9775529
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-09-28 15:36:34 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -32,15 +32,9 @@ async def start_command(client: Client, message: Message):
                  InlineKeyboardButton("Update Channel", url=f"t.me/{force_channel}")
                  ]]
                 )
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
-                )
-            ]
-        )
             )
+            return
+            
     id = message.from_user.id
     if not await present_user(id):
         try:
