445d416e5387a3ecb54c5af055c8dbbd62939326
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-09-28 19:40:03 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -50,8 +50,9 @@ async def start_command(client: Client, message: Message):
                  ]]
                 )
             )
-            return       
-   if not await present_user(id):
+            return
+    id = message.from_user.id          
+    if not await present_user(id):
         try:
             await add_user(id)
         except:
