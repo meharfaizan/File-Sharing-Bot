2977fc44d97e4600c8ae9cee96703864ae82d47b
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-09-28 13:48:05 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
-from pyrogram import UserNotParticipant
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
