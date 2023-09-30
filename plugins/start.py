f75495ec65107881e7cd7bc5b5e3d9e34f6f49b6
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-09-30 19:50:18 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -144,7 +144,27 @@ WAIT_MSG = """"<b>Processing ...</b>"""
 REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
 
 #=====================================================================================##
-
+force_sub="masamunekunsrevengex"
+@Bot.on_message(filters.command('start') & filters.private & subscribed)
+async def start_command(client: Client, message: Message):
+    if force_sub:
+        try:
+            user = await client.get_chat_member(force_sub, message.from_user.id)
+            if user.status == 'kicked out':
+                await message.reply_text("You are banned")
+                return
+        except UserNotParticipant:
+            await message.reply_text(
+                text="You are not subscribed to @masamunekunsrevengex",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel}")
+                        ]
+                    ]
+                ),
+            )
+            return
     
     
 @Bot.on_message(filters.command('start') & filters.private)
@@ -180,27 +200,6 @@ async def not_joined(client: Client, message: Message):
         quote = True,
         disable_web_page_preview = True
     )
-force_sub="masamunekunsrevengex"
-@Bot.on_message(filters.command('start') & filters.private & subscribed)
-async def start_command(client: Client, message: Message):
-    if force_sub:
-        try:
-            user = await client.get_chat_member(force_sub, message.from_user.id)
-            if user.status == 'kicked out':
-                await message.reply_text("You are banned")
-                return
-        except UserNotParticipant:
-            await message.reply_text(
-                text="You are not subscribed to @masamunekunsrevengex",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel}")
-                        ]
-                    ]
-                ),
-            )
-            return
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
