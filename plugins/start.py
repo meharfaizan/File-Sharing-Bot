f050f110ed23949b19a71e90fea7a74dea6bb433
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-10-18 14:23:41 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,8 +20,8 @@ from database.database import add_user, del_user, full_userbase, present_user
 # Define the channel IDs
 FORCE_SUB_CHANNEL_IDS = {
     "channel": "AnimeColony",
-    "channel2": "The_Kingdoms_of_RuinDub",
-    "channel3": "spyxfamilydualaudio480p",
+    "channel2": "GoblinSlayerdub",
+    "channel3": "imGivingthedisgracedNobleladydub",
     "channel4": "EminenceinShadowDub",
     "channel4": "ShangriLaFrontie",
     "channel5": "risingofshieldheroseason2dual",
@@ -37,7 +37,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="You are not Subscribed to these Channels @EminenceinShadowDub @risingofshieldheroseason2dual @ShangriLaFrontie   @spyxfamilydualaudio480p  @AnimeColony @The_Kingdoms_of_RuinDub",
+                text="You are not Subscribed to these Channels @EminenceinShadowDub @risingofshieldheroseason2dual @ShangriLaFrontie   @imGivingthedisgracedNobleladydub  @AnimeColony @GoblinSlayerdub ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
