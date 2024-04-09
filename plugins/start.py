b7d851dcc992d5ed1bbff864c78132c204251858
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2024-04-09 17:40:01 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -40,7 +40,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="You are not Subscribed to these Channels @EminenceinShadowDub @animecolony @ninjakamuidual @banishedfromtheherospartydub ",
+                text="You are not Subscribed to these Channels @EminenceinShadowDub @animecolony @joblessreincarnationx ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
