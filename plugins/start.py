88e801e6faf7d0dafc5711bbd45ce961920897dd
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2024-07-27 18:56:58 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -41,7 +41,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="Thanks for Joining ☺️ @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime @EminenceinShadowDub_1 @animecolony @joblessreincarnationx @AlyaSometimesHidesHerFeelingsdub @MagicalGirlandtheEvilLieutenant1 @MyInstantDeathAbilityAv1 @unwantedundeadadventurer @TOWER_OF_GOD_Eng_Dub_1 ",
+                text="Thanks for Joining ☺️ @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @EminenceinShadowDub_1 \n @animecolony \n @joblessreincarnationx \n @AlyaSometimesHidesHerFeelingsdub \n @MagicalGirlandtheEvilLieutenant1 \n @MyInstantDeathAbilityAv1 \n @unwantedundeadadventurer \n @TOWER_OF_GOD_Eng_Dub_1 ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
