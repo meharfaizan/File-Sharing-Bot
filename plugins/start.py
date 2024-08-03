8bb11f62d781605b0bf7b0614fc5d5afe893d518
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2024-08-03 20:59:39 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,12 +20,12 @@ from database.database import add_user, del_user, full_userbase, present_user
 # Define the channel IDs
 FORCE_SUB_CHANNEL_IDS = {
     "channel": "FZAnime",
-    "channel2": "MyInstantDeathAbilityAv1",
-    "channel3": "unwantedundeadadventurer",
+#    "channel2": "MyInstantDeathAbilityAv1",
+    "channel3": "Animes_Paradox",
   #  "channel25": "YouWereExperiencedIWasNotdub",
     "channel33": "AlyaSometimesHidesHerFeelingsdub",
     "channel24":  "TOWER_OF_GOD_Eng_Dub_1",
-    "channel4":  "MagicalGirlandtheEvilLieutenant1"}
+#    "channel4":  "MagicalGirlandtheEvilLieutenant1"}
   #  "channel224":  "TOWER_OF_GOD_Eng_Dub_1",
    # "channel7": "plutoanimedub",
  #   "channel5": "risingofshieldheroseason2dual",
@@ -41,7 +41,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="Thanks for Joining ☺️ @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @EminenceinShadowDub_1 \n @animecolony \n @FZAnime \n @joblessreincarnationx \n @AlyaSometimesHidesHerFeelingsdub \n @MagicalGirlandtheEvilLieutenant1 \n @MyInstantDeathAbilityAv1 \n @unwantedundeadadventurer \n @TOWER_OF_GOD_Eng_Dub_1 ",
+                text="Thanks for Joining ☺️ @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @Animes_Paradox \n @animecolony \n @FZAnime \n @MyInstantDeathAbilityAv1 \n @unwantedundeadadventurer \n @TOWER_OF_GOD_Eng_Dub_1 ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
