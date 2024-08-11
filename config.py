d49ddcd7e542b40a91c37fc19d617b11c895dade
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2024-08-11 17:21:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -71,4 +71,20 @@ ADMINS.append(1250450587)
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
-    level=logging.
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
