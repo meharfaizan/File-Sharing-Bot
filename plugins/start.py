99665180115356a5487c3cdab0550c68f7b350dc
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2024-09-13 15:19:13 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -34,7 +34,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="You are not Subscribed to these Channels @ReMonsterDub \n @oshinokodubseries \n @Animedualaudiozippercartoonist ",
+                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels @ReMonsterDub \n @oshinokodubseries \n @Animedualaudiozippercartoonist ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
