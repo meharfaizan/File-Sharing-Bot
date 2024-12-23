3ac30e7be51b815f6971aaa1659672d55a98b664
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2024-12-23 18:22:37 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -22,7 +22,7 @@ FORCE_SUB_CHANNEL_IDS = {
     "channel2": "arifureta_english_dubbed_1",
     "channel3": "rezero_english_dubb",
     "channel4": "RascalDoesNotDreamofBunnyGirl_1",
-    "channel5": "bluebox_engllish_dubbed",
+    "channel5": "bluelockdubenglishdubbed",
 #    "channel6": "MeijiGekken1874dub",
 }
 # Add a handler for the /start command
@@ -36,7 +36,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @rezero_english_dubb \n @RascalDoesNotDreamofBunnyGirl_1 \n @arifureta_english_dubbed_1 \n @lonerlifeinanotherworld_dub \n @Ranma1_2EnglishDubbed \n @bluebox_engllish_dubbed \n @animecolony_1 \n ",
+                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @rezero_english_dubb \n @RascalDoesNotDreamofBunnyGirl_1 \n @arifureta_english_dubbed_1 \n @lonerlifeinanotherworld_dub \n @Ranma1_2EnglishDubbed \n @bluelockdubenglishdubbed \n @animecolony_1 \n ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
