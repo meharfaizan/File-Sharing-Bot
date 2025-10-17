e1f68646254d1fa74024c32dffb38daf8a8e5f3a
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2025-10-17 11:54:40 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,7 +18,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 FORCE_SUB_CHANNEL_IDS = {
     "channel": "Animedualaudiozippercartoonist",
     "channel2": "TgCryptHub",
-    "channel3": "animedualaudiodub",
+  #  "channel3": "animedualaudiodub",
     "channel4": "anidbs",
     "channel5": "themodsnjbhvgiteeyhtheMods2025",
     "channel7": "PVCRYPTO0",
@@ -36,7 +36,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels \n @animedualaudiodub \n @PVCRYPTO0 \n @themodsnjbhvgiteeyhtheMods2025 \n @megasforex \n @TgCryptHub  \n @Animedualaudiozippercartoonist \n @anidbs",
+                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels \n @PVCRYPTO0 \n @themodsnjbhvgiteeyhtheMods2025 \n @megasforex \n @TgCryptHub  \n @Animedualaudiozippercartoonist \n @anidbs",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
